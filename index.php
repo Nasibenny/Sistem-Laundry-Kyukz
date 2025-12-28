6ec44f652fdf0929d4806bc47a6b9568d64c46f5
Benny <koropirate18@gmail.com>
2025-12-28 09:49:34 +0800

    feat:login page

--- index.php
+++ index.php
@@ -1,207 +1,67 @@
 <!doctype html>
-<?php
-include 'koneksi.php';
-session_start();
-?>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>LaundryKyuk | laundry</title>
-    <!-- Online -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <!-- Offline -->
-    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
-    <link rel="icon" type="image/png" href="./img/laundry-hand.svg">
-
-    <!-- Asset -->
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    
-    <link rel="stylesheet" href="./style/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-   
-    <link rel="stylesheet" href="./style/style.css">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+  
+    <link rel="icon" type="image/png" href="./img/island_wht.svg">
   </head>
-  <body>
-
-  <div class="">
-    <h1>TESTT</h1>
-  </div>
-    <div class="wrapper"> 
-    <!-- Navbar -->
-    <aside id="sidebar" class="py-2 " >
-    
-        <div class="d-flex">
-          <button id="toggle-btn" type="button">
-                <!-- <i class="lni lni-island"></i> -->
-                <img src="./img/laundry-hand.svg" width="40px" alt="">
-          </button>
-    <!-- Logo -->
-          <div class="sidebar-logo">
-              <a href="#">LaundryKyuk</a>
-          </div>
-        </div>
-    <!-- Logo -->
-        <ul class="sidebar-nav "  id="sidebar-active" >
-          <li class="sidebar-item ">
-              <a href="dashboard.php?page=dashboard" class="sidebar-link  ">
-                  <i class="lni lni-home"></i>
-                  <span>Home</span>
-              </a>
-          </li>
-    <!-- Menu -->
-   
-          <li class="sidebar-item">
-              <a href="#" class="sidebar-link has-dropdown collapsed dropdown-toggle"  data-bs-toggle="collapse" data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
-                  <i class="lni lni-package"></i>
-                  <span>Data Master</span>
-              </a>
-
-              <ul id="auth" class="sidebar-dropdown list-unstyled collapse"  data-bs-parent="#sidebar ">
-                <li class="sidebar-item">
-                   <a class="sidebar-link" href="dashboard.php?page=paket">Paket</a>
-                </li>
-                <li class="sidebar-item">
-                   <a class="sidebar-link" href="dashboard.php?page=outlet">Outlet</a>
-                </li>
-              </ul>
-          </li>
-
-            <li class="sidebar-item">
-              <a href="dashboard.php?page=member" class="sidebar-link"  >
-                   <i class="lni lni-users"></i>
-                  <span>Registrasi Pelanggan</span>
-              </a>
-          </li>
-          <li class="sidebar-item">
-              <a href="dashboard.php?page=tambah_transaksi" class="sidebar-link "  >
-                  <i class="lni lni-dollar"></i>
-                  <span>Entri transaksi</span>
-              </a>
-          </li>
-         
-          <li class="sidebar-item">
-              <a href="dashboard.php?page=laporan" class="sidebar-link "  >
-                  <i class="lni lni-remove-file"></i>
-                  <span>Laporan</span>
-              </a>
-          </li>
+  
+  <style>
+    body{
+      margin:0px;
+      padding:0px;
+      background-color:#F5F5F5;
       
-       
-        </ul>
-        <div class="sidebar-footer">
-            <a href="logout.php" class="sidebar-link">
-              <i class="lni lni-exit"></i>
-              <span>Logout</span>
-            </a>
-        </div>
-        
-      </aside>
-    
-      <div class="main ">
-          <!-- Navbar Atas  -->
-          <nav class=" navbar navbar-expand px-5 py-3 bg-custom ">          
-                  <ul class="navbar-nav ms-auto ">
-                      <li class="">
-                          <a href="#" class="nav-icon pe-md-0" data-bs-toggle="dropdown">
-                            
-                              <img src="./img/user.png" class="avatar img-fluid" alt="">
-                              
-                          </a>
-                      </li>
-                      <li class="text-center ">
-                        <?php
-                        // $tampil = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT role FROM tb_user WHERE id_user=' $_SESSION[role]' "));
-                        ?>
-            
+      font-family: 'Poppins', sans-serif;
+    }
+  </style>
+<body >
 
-                        <!-- <h6 class="fw-bold text-capitalize "><?= $_SESSION['username'] ?></h6> <h6 class="text-capitalize"><?= $_SESSION['role'] ?></h6> -->
-                        <div class="">
-
-                            <h6 class="fw-medium  text-capitalize text-white"> Admin</h6>
-                            <h6 class="text-capitalize text-white">Messi</h6>
-                        </div>
-                      </li>
-                  </ul>
-             
-          </nav>
-          <!-- Navbar Atas  -->
-     
-          <!-- Content Dashboard  -->
-          <main class="content px-3 py-4 ">
-           
-           
-              <div class="container">
-             
-                       
-                <?php
-                //laundry_beni/dashboard.php?page=(nama page)
-                switch(@$_GET['page']){
-                case "dashboard":
-                    include_once 'view/view_home.php';
-                break;
-                case "outlet":
-                    include_once 'view/view_outlet.php';
-                break;
-                case "tambah_outlet":
-                    include_once 'tambah/tambah_outlet.php';
-                break;
-                case "edit_outlet":
-                    include_once 'edit/edit_outlet.php';
-                break;
-                case "delete_outlet":
-                    include_once 'tambah/delete_outlet.php';
-                break;
-                //CASE  MEMBER
-                case"member";
-                include_once 'view/view_member.php';
-                break;
-                case "tambah_member":
-                include_once 'tambah/tambah_member.php';
-                break;
-                case"edit_member";
-                include_once 'edit/edit_member.php';
-                break;
-                //CASE PAKET
-                case"paket";
-                include_once 'view/view_paket.php';
-                break;
-                case"tambah_paket";
-                include_once 'tambah/tambah_paket.php';
-                break;
-                case"edit_paket";
-                include_once 'edit/edit_paket.php';
-                break;
-                //CASE TRANSAKSI
-                case"tambah_transaksi";
-                include_once 'tambah/tambah_transaksi.php';
-                break;
-                case"detail_transaksi";
-                include_once 'tambah/detail_transaksi.php';
-                break;
-                //CASE LAPORAN
-                case"laporan";
-                include_once 'view/view_laporan.php';
-                break;
-               
-                }
-               
-                ?>
-              </div>
-          </main>
-          <!-- Footer -->
-          
+<div class="container min-vh-100 d-flex flex-column justify-content-center align-items-center ">
+      <div class="text-center mb-4">
+        <img class="mb-2" src="./img/island.svg" style="width:100px" alt="">
+        <h1 class="fs-3 text-center fw-bold">Sign in</h1>
       </div>
-    </div>
 
+  <div class=" ">
 
-   <script src="./js/script.js"></script>             
-   <!-- Online -->           
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-   <!-- Offline -->
-   <script src="./bootstrap/js/bootstrap.min.js"></script>  
-    <!--ICON LINK-->
-   <script src="https://kit.fontawesome.com/f143f4fbae.js" crossorigin="anonymous"></script>
+    <form action="proses_login.php" method="post">
+
+      <div class=" form-floating mb-4 ">
+        
+          <input required type="name" name="username" class="form-control px-5 " id="floatinginput">
+          <label class="text-secondary" for="floatingInput">Username</label>
+      </div>
+      <div class="  form-floating mb-4">
+       
+        <input required type="password" name="password" class="form-control px-5" id="floatingpassword"  >
+        <label  class="text-secondary" for="floatingpassword">Password</label>
+      </div>
+  
+         <button type="submit" class="btn btn-primary w-100 py-2 fs-6">Sign in</button>
+          <div  class="form-text text-center text-secondary mb-5 mt-3">Forgot yout password?</div>
+     </form>
+      <div class="">
+        <h1 class="fs-6 text-center  mb-4">Don't have a Column account?</h1>
+        <a href="./register/register.php" class=" shadow-sm  btn btn-light border border-2 w-100 py-2 fs-6">Create an Account</a>
+      </div>
+  </div>
    
-  </body>
+  
+
+ 
+
+        
+   
+</div>
+
+</body>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
---
+++ view/view_home.php
@@ -0,0 +1,227 @@
+<?php
+include 'koneksi.php';
+
+?>
+<div class="container pl-5">
+  <div class="mb-3">
+     <?php
+              $tampil = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT nama FROM tb_outlet WHERE id_outlet=' $_SESSION[id_outlet]' "));
+      ?>
+            
+      <div class="d-flex gap-1 pb-3">
+          <h5 class=" fs-4 mb-3"> Selamat Datang </h5>
+
+          <h5 class="fw-bold fs-4 text-decoration-underline"> <?php echo $tampil["nama"] ?> </h5>
+
+      </div>
+
+            <div class="row">
+              <!-- Proggres member -->
+                  <div class="col-12 col-md-4">
+                        <div class="card mb-3" style="max-width: 540px;">
+                          <div class="row g-0">
+                            <div class="col-md-4 d-flex justify-content-center">
+                              <!-- <i class="lni lni-shopping-basket"></i>   -->
+                            <img src="./img/shopping-basket.svg" class="w-50" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                              <div class="card-body">
+                                <h5 class="card-title">Pesanan Laundry</h5>
+                                    <?php
+                                        $pesan_londre = mysqli_num_rows(mysqli_query($koneksi,"SELECT id_transaksi FROM tb_detail_transaksi WHERE id_detail"));
+                                        // $tampil = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT id_transaksi FROM tb_detail_transaksi WHERE id_detail "));
+                                    ?>
+                                <p class="card-text"><?php echo $pesan_londre?> </p>
+                                <p class="card-text">
+                                  <small class="text-body-secondary"> </i >Last updated <span class="fw-bold">
+
+                                     <?php  
+                                      if(isset($_SESSION['idtransaksi'])) {
+                                          $pesan_waktu = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT tgl FROM tb_transaksi WHERE id_transaksi='$_SESSION[idtransaksi]'"));
+                                          if($pesan_waktu && isset($pesan_waktu['tgl'])) {
+                                              echo "<span class='fw-bold'>" . $pesan_waktu['tgl'] . "</span>";
+                                          } else {
+                                              echo "Belum";
+                                          }
+                                      } else {
+                                          echo "Belum Update";
+                                      }
+                                      ?> 
+                                
+                                  </span>  
+                                  </small></p>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+              <!-- Keuntungan -->
+                    <div class="col-12 col-md-4">
+                     <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4 d-flex justify-content-center">
+                            <img src="./img/stats-up.svg" class="w-50" alt="...">
+                          </div>
+                          <div class="col-md-8 ">
+                            <div class="card-body">
+                              <?php
+                                         
+                                        $grand = mysqli_fetch_row(mysqli_query($koneksi, "SELECT SUM(total_harga) FROM tb_detail_transaksi INNER JOIN tb_paket ON tb_detail_transaksi.id_paket=tb_paket.id_paket WHERE id_transaksi"));
+                                        // $tampil = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT total_harga FROM tb_detil_transaksi WHERE id_detail "));
+                                    ?>
+                              <h5 class="card-title">Total Pendapatan</h5>
+                              <?php
+                                        if($grand['0'] == null){
+                                            $grand['0'] = 0;
+                                        }
+                                ?>
+
+                              <p class="card-text">Rp. <?php echo number_format($grand['0'],0,',','.'); ?></p>
+                              <p class="card-text"><small class="text-body-secondary">
+                                Last updated <span class="fw-bold">
+                                <?php  
+                                      if(isset($_SESSION['idtransaksi'])) {
+                                          $pesan_waktu = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT tgl FROM tb_transaksi WHERE id_transaksi='$_SESSION[idtransaksi]'"));
+                                          if($pesan_waktu && isset($pesan_waktu['tgl'])) {
+                                              echo "<span class='fw-bold'>" . $pesan_waktu['tgl'] . "</span>";
+                                          } else {
+                                              echo "Belum";
+                                          }
+                                      } else {
+                                          echo "Belum Update";
+                                      }
+                                      ?> 
+                              
+                                    </span></small></p>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+              <!-- Keuntungan -->
+                    <div class="col-12 col-md-4">
+                      <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                          <div class="col-md-4 d-flex justify-content-center">
+                            <img src="./img/users.svg" class="w-50" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">Pelanggan</h5>
+                                  <?php
+                                        $pelanggan = mysqli_num_rows(mysqli_query($koneksi,"SELECT id_member FROM tb_member WHERE id_member"));
+                                        // $tampil = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT id_transaksi FROM tb_detail_transaksi WHERE id_detail "));
+                                    ?>
+                              <p class="card-text"><?php echo $pelanggan?> </p>
+                              <p class="card-text"><small class="text-body-secondary">
+                                Last updated <span class="fw-bold">
+                                <?php  
+                                      if(isset($_SESSION['idtransaksi'])) {
+                                          $pesan_waktu = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT tgl FROM tb_transaksi WHERE id_transaksi='$_SESSION[idtransaksi]'"));
+                                          if($pesan_waktu && isset($pesan_waktu['tgl'])) {
+                                              echo "<span class='fw-bold'>" . $pesan_waktu['tgl'] . "</span>";
+                                          } else {
+                                              echo "Belum";
+                                          }
+                                      } else {
+                                          echo "Belum Update";
+                                      }
+                                      ?> 
+                             </span> </small></p>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+
+                    
+                  
+                  
+            </div>
+     
+      <h3 class=" fs-4 my-3">Transaksi Terkahir</h3>
+
+            <div class="row">
+              <!-- Table -->
+                <div class="col-12">
+                      <?php
+                            $sql = "SELECT id_outlet, COUNT(*) AS total_transactions
+                                      FROM tb_transaksi
+                                      GROUP BY id_outlet
+                                      ORDER BY total_transactions DESC";
+                              $result = mysqli_query($koneksi, $sql);
+
+                              $labels = [];
+                              $data = [];
+
+                              while ($row = mysqli_fetch_assoc($result)) {
+                                  $labels[] = "Outlet " . $row['id_outlet'];
+                                  $data[] = $row['total_transactions'];
+                              }
+                        ?>
+            
+                            <!-- Card 1 for Bar Chart -->
+                           
+                                
+                                <div class="relative">
+                                    <canvas id="transactionChart1" width="500" height="130"></canvas>
+                                </div>
+                            
+
+
+                      <script>
+                          const labels = <?php echo json_encode($labels); ?>;
+                          const data = <?php echo json_encode($data); ?>;
+
+                          // Configuration for the bar chart
+                          const config1 = {
+                              type: 'bar',
+                              data: {
+                                  labels: labels,
+                                  datasets: [{
+                                      label: 'Total Transaction Per Month',
+                                      data: data,
+                                      backgroundColor: [
+                                          'rgba(255, 99, 132, 0.2)',
+                                          'rgba(54, 162, 235, 0.2)',
+                                          'rgba(255, 206, 86, 0.2)',
+                                          'rgba(75, 192, 192, 0.2)',
+                                          'rgba(153, 102, 255, 0.2)',
+                                          'rgba(255, 159, 64, 0.2)'
+                                      ],
+                                      borderColor: [
+                                          'rgba(255,99,132,1)',
+                                          'rgba(54, 162, 235, 1)',
+                                          'rgba(255, 206, 86, 1)',
+                                          'rgba(75, 192, 192, 1)',
+                                          'rgba(153, 102, 255, 1)',
+                                          'rgba(255, 159, 64, 1)'
+                                      ],
+                                      borderWidth: 1
+                                  }]
+                              },
+                              options: {
+                                  responsive: true,
+                                  plugins: {
+                                      legend: {
+                                          position: 'top',
+                                      },
+                                      title: {
+                                          display: true,
+                                          text: 'Total Transactions'
+                                      }
+                                  }
+                              },
+                          };
+
+                        
+                          // Initialize the bar chart
+                          const myBarChart = new Chart(
+                              document.getElementById('transactionChart1'),
+                              config1
+                          );
+                </script>          
+  
+                </div>
+            </div>
+      </div>
+</div>
